d4059262a5e416d51428ff64bab41f0bb2cae09c
Jordan Allspaw <allspaw.j@gmail.com>
2025-01-30 12:13:15 -0500

    added some failsafes for bad bags

--- src/bag_recorder.py
+++ src/bag_recorder.py
@@ -16,33 +16,39 @@ from std_srvs.srv import Empty, EmptyResponse
 
 class RosbagRecorder:
     def __init__(self):
-        self.bag = rosbag.Bag('/bags/output.bag', 'w')
         rospy.init_node('rosbag_recorder', anonymous=True)
         start_service = rospy.Service('/start_bag', Empty, self.start_record)
         stop_service = rospy.Service('/stop_bag', Empty, self.stop_record)
         rospy.on_shutdown(self.shutdown_hook)
 
     def jointStateCallback(self, msg):
-        self.bag.write('/joint_states', msg)
+        if self.write_to_bag:
+            self.bag.write('/joint_states', msg)
         
     def pointCloudCallback(self, msg):
-        self.bag.write('/head_camera/depth_registered/points/filtered/throttled/', msg)
+        if self.write_to_bag:
+            self.bag.write('/head_camera/depth_registered/points/filtered/throttled/', msg)
         
     def laserCallback(self, msg):
-        self.bag.write('/base_scan', msg)
+        if self.write_to_bag:
+            self.bag.write('/base_scan', msg)
         
     def imageCallback(self, msg):
-        self.bag.write('/head_camera/rgb/image_raw/compressed', msg)
+        if self.write_to_bag:
+            self.bag.write('/head_camera/rgb/image_raw/compressed', msg)
     
     def goalCallback(self, msg):
-        self.bag.write('/gripper_goal/current', msg)
+        if self.write_to_bag:
+            self.bag.write('/gripper_goal/current', msg)
     
     def shutdown_hook(self):
         rospy.loginfo("Shutting down and closing bag file.")
-        self.bag.close()
+        #self.bag.close()
         
     def start_record(self, req):
         rospy.loginfo("Recording to output.bag")
+        self.write_to_bag = True
+        self.bag = rosbag.Bag('/bags/output.bag', 'w')
         self.joint_state_sub =  rospy.Subscriber('/joint_states', JointState, self.jointStateCallback)
         self.point_cloud_sub =  rospy.Subscriber('/head_camera/depth_registered/points/filtered/throttled', PointCloud2, self.pointCloudCallback)
         self.laser_scan_sub =  rospy.Subscriber('/base_scan', LaserScan, self.laserCallback)
@@ -52,6 +58,7 @@ class RosbagRecorder:
 
     def stop_record(self, req):
         rospy.loginfo("Stopping recording to output.bag")
+        self.write_to_bag = False
         self.joint_state_sub.unregister()
         self.point_cloud_sub.unregister()
         self.laser_scan_sub.unregister()
