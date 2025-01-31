31c3a292288d4e640d42e141e9ae43d92ae4baba
Jordan Allspaw <allspaw.j@gmail.com>
2025-01-31 13:49:47 -0500

    bag replay now handles time events better

--- src/bag_replay.py
+++ src/bag_replay.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from std_msgs.msg import Int32
 from std_msgs.msg import String
 from std_msgs.msg import Time
+from std_msgs.msg import Float32
 import os
 
 repub_topics = [
@@ -13,19 +14,19 @@ repub_topics = [
     '/head_camera/rgb/image_raw/compressed',
     '/head_camera/depth_registered/points/filtered/throttled',
     '/base_scan',
-    '/gripper_goal/current',
+    #'/gripper_goal/current',
     '/joint_states'
     ]
 current_replay = "output.bag"
+current_duration = 0
 
 list_of_replays = []
 current_time = 0.0
+replay_speed = 0.05
 #current_duration = 1666048230 - 1666048219
 
-#bag_start = 1666048219
-#bag_end = 1666048230
-
 replay_stop = "stop"
+replay_pause = "pause"
 replay_play = "play"
 
 replay_mode = "stop"
@@ -36,19 +37,27 @@ def set_current_mode(msg):
     rospy.loginfo("Setting current replay mode to")
     rospy.loginfo(msg.data)
     replay_mode = msg.data
-    current_time = 0
+    #current_time = 0
 
 def set_current_time(msg):
-    global current_time
+    global current_time, replay_mode, replay_pause
     rospy.loginfo("Setting current replay time to")
     rospy.loginfo(msg)
     current_time = msg.data#.secs
+    replay_mode = replay_pause
+    
+def set_current_speed(msg):
+    global replay_speed
+    rospy.loginfo("Setting current replay speed to")
+    rospy.loginfo(msg)
+    replay_speed = msg.data
     
 def set_current_replay(msg):
-    global current_replay
+    global current_replay, current_time
     rospy.loginfo("Setting Active replay ID to")
     rospy.loginfo(msg)
     current_replay = msg.data
+    current_time = 0
     if current_replay in list_of_replays:
         load_bag("/bags/" + current_replay)
     else:
@@ -56,24 +65,37 @@ def set_current_replay(msg):
     
     
 def load_bag(bag_file):
-    global bag_start, bag_end, current_duration
+    global bag_start, bag_end, current_duration, current_time
     bag = rosbag.Bag(bag_file)
     bag_start = bag.get_start_time()
     bag_end = bag.get_end_time()
     current_duration = bag_end - bag_start
+    #current_time = 0
     
     for topic, msg, t in bag.read_messages( topics="/gripper_goal/current"):
         pub = rospy.Publisher("/replay" +topic, type(msg), queue_size=100)
         rospy.loginfo("We got a gripper goal!")
         break
-    rospy.loginfo(current_duration)
+    return bag
 
-def replay_bag(bag_file, _start_time, _end_time):
-    global replay_mode, replay_stop, replay_play, current_time
-    bag = rosbag.Bag(bag_file)
-    #current_duration = bag.get_end_time() - bag.get_start_time()
-    #rospy.loginfo(current_duration)
-    _start_time = rospy.Time.from_sec(bag.get_start_time())
+def publish_data():
+    global current_time, current_duration, list_of_replays
+    list_of_replays_msg = ""
+    for i in range(0, len(list_of_replays)):
+        list_of_replays_msg+=list_of_replays[i] + ","
+    current_time_msg = Time()
+    current_time_msg.data.secs = current_time
+    time_pub.publish(current_time_msg)
+    
+    current_duration_msg = Time()
+    current_duration_msg.data.secs = current_duration
+    duration_pub.publish(current_duration_msg)
+    list_of_replays_pub.publish(list_of_replays_msg)
+
+def replay_bag(bag_file):
+    global replay_mode, replay_stop, replay_play, current_time, replay_speed, bag_start, bag_end
+    bag = load_bag(bag_file)
+    _start_time = rospy.Time.from_sec(bag.get_start_time() + current_time)
     _end_time = rospy.Time.from_sec(bag.get_end_time())
     #_start_time += current_time
     if(_end_time < _start_time):
@@ -81,21 +103,24 @@ def replay_bag(bag_file, _start_time, _end_time):
         bag.close()
         return
     #start_time=_start_time, end_time=_end_time,
-    for topic, msg, t in bag.read_messages( topics=repub_topics):
+    for topic, msg, t in bag.read_messages( topics=repub_topics, start_time=_start_time, end_time=_end_time):
         if rospy.is_shutdown():
             rospy.loginfo("Shutdown signal received")
             bag.close()
             break
-        if replay_mode == replay_stop:
+        if replay_mode == replay_stop or replay_mode == replay_pause:
             rospy.loginfo("Replay mode is stop")
             bag.close()
             break
-        rospy.loginfo("Replaying message on topic ")
+        #rospy.loginfo("Replaying message on topic ")
         rospy.loginfo(topic)
-        rospy.loginfo(t.to_sec())
+        #rospy.loginfo(t.to_sec())
+        if topic == "/base_scan" : #hack
+           publish_data() 
         pub = rospy.Publisher("/replay" +topic, type(msg), queue_size=100)
         pub.publish(msg)
-        rospy.sleep(0.01)  # Adjust sleep time as needed
+        rospy.sleep(replay_speed)  # Adjust sleep time as needed
+        current_time = t.to_sec() - bag_start
     replay_mode = replay_stop
     bag.close()
     
@@ -112,6 +137,7 @@ if __name__ == "__main__":
     rospy.Subscriber('/set_current_replay', String, set_current_replay)
     rospy.Subscriber('/set_current_time', Int32, set_current_time)
     rospy.Subscriber('/set_replay_mode', String, set_current_mode)
+    rospy.Subscriber('/set_replay_speed', Float32, set_current_speed)
     
     #rospy.Service()
     
@@ -135,26 +161,13 @@ if __name__ == "__main__":
         if(list_files("/bags") != list_of_replays):
             print("New bag detected, reloading")
             list_of_replays = list_files("/bags")
-            list_of_replays_msg = ""
-            for i in range(0, len(list_of_replays)):
-                list_of_replays_msg+=list_of_replays[i] + ","
             
-        current_time_msg = Time()
-        current_time_msg.data.secs = current_time
-        time_pub.publish(current_time_msg)
-        
-        current_duration_msg = Time()
-        current_duration_msg.data.secs = current_duration
-        duration_pub.publish(current_duration_msg)
-        list_of_replays_pub.publish(list_of_replays_msg)
-
-        if replay_mode == replay_play and current_time < current_duration:
-            rospy.loginfo("Replaying bag")
-            rospy.loginfo(list_of_replays)
-            replay_bag("/bags/" + current_replay, bag_start , bag_start)
-            current_time += 1
-        else:
+        publish_data()
+        if replay_mode == replay_play:
+            replay_bag("/bags/" + current_replay)
+            #current_time += 1
+        #else:
             #rospy.loginfo("Not replaying bag")
-            replay_mode = replay_stop
+        #    replay_mode = replay_stop
         rate.sleep()
 #``` close_bag()
